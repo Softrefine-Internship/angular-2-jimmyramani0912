5f345c33d672d1043ffdf0a70d2a9af30ef7c989
Jimmy Ramani <jimmy.softrefine@gmail.com>
2025-02-14 16:07:05 +0530

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-2';
+  @ViewChild('myContainer') myContainer!: ElementRef;
+
+  onBgColorChange(event: Event) {
+    const color = (<HTMLInputElement>event.target).value;
+    document.body.style.backgroundColor = color;
+  }
+
+  onTextColorChange(event: Event) {
+    const color = (<HTMLInputElement>event.target).value;
+    this.myContainer.nativeElement.style.color = color;
+  }
 }
